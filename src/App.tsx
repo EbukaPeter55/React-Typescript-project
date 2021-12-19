4fd2ea18d77bea1cc61aac4051deb2007581bff7
ebuka <peterdbrainy5@gmail.com>
2021-12-19 23:01:10 +0100

    created a component directory and list component for card. save the object array in its state in appjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
 
+
+interface IState {
+   people: {
+     name: string,
+     age: number,
+     url: string,
+     note?: string//string type is optional
+   }[]
+}
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+    name: 'Lebron James',
+    url: '',
+    age: 36,
+    note: 'Allergic to staying on the same team.'
+  },
+  {
+    name: 'Kobe Bryant',
+    url: '',
+    age: 36,
+   },
+
+])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to the party</h1>
+      <List/>
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const List = () => {
+    return (
+        <div>
+            I am a list
+        </div>
+    )
+}
+
+
+export default List;
